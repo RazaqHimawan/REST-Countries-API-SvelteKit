e651c81cfe23d053d47b4408a23902030e51bb2a
RazaqHimawan <razaqhim@gmail.com>
2023-06-16 18:53:43 +0700

    use netlify

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/api-endpoint.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "https://restcountries.com/v3.1";
-const API_ENDPOINT = {
-  ALL: `${BASE_URL}/all`,
-  REGION: (region) => `${BASE_URL}/region/${region}`,
-  DETAIL: (code) => `${BASE_URL}/alpha/${code}`
-};
-export {
-  API_ENDPOINT as A
-};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,9 +0,0 @@
-import * as server from '../entries/pages/_page.server.js';
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export { server };
-export const server_id = "src/routes/+page.server.js";
-export const imports = ["_app/immutable/nodes/2.554e9a93.js","_app/immutable/chunks/index.62ec8c34.js","_app/immutable/chunks/index.bd632bad.js"];
-export const stylesheets = ["_app/immutable/assets/2.1ccd88f2.css"];
-export const fonts = [];
